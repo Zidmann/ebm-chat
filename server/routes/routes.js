a5e3c463fe947858e9c136a306d1267f07651fa6
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2014-03-13 21:54:32 +0100

    Updates of some routes.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,35 @@
 var register=function (app,ctrl)
 {
-	app.get("/users",ctrl.user.get);
         app.get('/', function(req, res) {
              res.setHeader('Content-Type', 'text/plain');
              res.end('Vous êtes à l\'accueil, que puis-je pour vous ?');
         });
+
+        app.get   ("/users", ctrl.user.get);
+        app.post  ("/users", ctrl.user.get);
+        app.put   ("/users", ctrl.user.get);
+        app.delete("/users", ctrl.user.get);
+
+        app.get   ("/users/:id", ctrl.user.get);
+        app.post  ("/users/:id", ctrl.user.get);
+        app.put   ("/users/:id", ctrl.user.get);
+        app.delete("/users/:id", ctrl.user.get);
+
+        app.get   ("/login/:door", ctrl.user.get);
+        app.delete("/login",       ctrl.user.get);
+
+        app.get   ("/rooms", ctrl.user.get);
+        app.post  ("/rooms", ctrl.user.get);
+
+        app.get   ("/rooms/:id", ctrl.user.get);
+        app.put   ("/rooms/:id", ctrl.user.get);
+        app.delete("/rooms/:id", ctrl.user.get);
+
+        app.get   ("/file/:id", ctrl.user.get);
+        app.post  ("/file/:id", ctrl.user.get);
+        app.put   ("/file/:id", ctrl.user.get);
+        app.delete("/file/:id", ctrl.user.get);
+
 }
 
 
