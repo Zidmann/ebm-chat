24daf70dfc8268a0fddf30fb5c47d84a0d2d472e
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2014-03-15 17:04:43 +0100

    Fix the path of the destination directory where uploaded files go

--- server/controllers/file.js
+++ server/controllers/file.js
@@ -1,6 +1,20 @@
+var Uploader = require('../lib/express-uploader/express-uploader');
+
 function uploadFile(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Function UserInfos');
+    var uploader = new Uploader({
+        debug: true,
+        validate: true,
+        thumbnails: true,
+        thumbToSubDir: true,
+        tmpDir: __dirname + '/../tmp',
+        publicDir: __dirname + '/../public',
+        uploadDir: __dirname + '/../public/files',
+        uploadUrl: '/files/',
+        thumbSizes: [140,[100, 100]]
+    });
+    uploader.uploadFile(req, function(data) {
+        res.send(JSON.stringify(data), {'Content-Type': 'text/plain'}, 200);
+    });
 }
 
 function destroyFile(req, res, next) {
