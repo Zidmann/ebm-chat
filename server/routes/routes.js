21c6142f6b0eb1ab00dcb74d2946a991b334e3da
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2014-03-16 13:45:19 +0100

    Some changes in routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,22 +1,24 @@
-var register=function (app,ctrl,cas)
+var register=function (app, ctrl, cas)
 {
 
-/* *
-   * START API DEFINITION
-   */
+/*
+ * START API DEFINITION
+ */
 //  Public APIs (do not require token to identificate)
+    app.post  ("/api/users",       ctrl.user.getConnected);   	        //Create a new user on database
     app.post  ("/api/login/:door", ctrl.login.signin);		        //Connect to chat server website through a door (Classical, Gmail, Facebook, CAS, ...)
 
 // Private APIs (require token to identificate user)
     app.delete("/api/login",       ctrl.login.signout);                 //Disconnect from chat server website
 
-    app.get   ("/api/users",       ctrl.user.getConnected);   	        //Get connected user information list
+    app.get   ("/api/users",       ctrl.user.getConnected);   	        //Get user list
     app.get   ("/api/users/:id",   ctrl.user.get);		        //Get information on a specific user
+    app.delete("/api/users/:id",   ctrl.user.get);		        //Get information on a specific user
 
     app.get   ("/api/rooms",     ctrl.room.get);		        //Get room list on the chat
     app.post  ("/api/rooms",     ctrl.room.create);		        //Create a new room on the chat
     app.get   ("/api/rooms/:id", ctrl.room.getMsg);		        //Get information and messages on a specific room
-    app.put   ("/api/rooms/:id", ctrl.room.postMsg);		        //Write a message in a specifi room
+    app.post  ("/api/rooms/:id", ctrl.room.postMsg);		        //Write a message in a specifi room
     app.delete("/api/rooms/:id", ctrl.room.destroy);		        //Destroy a specific room
 
     //File Services
