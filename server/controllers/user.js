703b140bc919de7f5944b8746de06ae402cbdf53
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2014-03-18 11:14:11 +0100

    Changes in user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,21 +1,84 @@
-function signup(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Function signup');
-}
+var 	User  = require('../models/user.js'),
+	Err   = require('../utils/err.js'),
+	_     = require('underscore'),
+	u     = require('../utils/utils.js'),
+	async = require('async');
 
+//GET USER FUNCTIONS
 function getUserInfos(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Function getUserInfos');
+	new User(req.params.id, function(err, user) {
+		if (err) return next(err);
+	
+		if (user.loaded) {
+			res.json(200, _.omit(user.obj(true), 'pass', 'isAdmin', 'created', 'updated'));
+		}
+		else next(new Err(404, 1102, "L'utilisateur n'existe pas"));
+	});
 }
 
+function getAll(req, res, next) {
+	User.getFromDb(function(err, t) {
+		if (err) return next(err);
+		var t = _.map(t, function(user) {
+			return _.omit(user.obj(true), 'pass', 'isAdmin');
+		});
+		res.json(200,t);
+	});
+}
+
+//CREATE USER FUNCTIONS
+function signup(req, res, next) {
+	var user = new User();
+
+	if (req.body.login == null || req.body.pass == null || req.body.firstName == null || req.body.lastName == null || req.body.mail == null)
+	{
+		return next(new Err(400, 1301, "Les paramètres envoyés ne sont pas complets"));
+	}	
+
+	user.login     = req.body.login;
+	user.setPassword(req.body.pass);
+        user.avatar    = req.body.avatar;
+	user.civilite  = req.body.civilite;
+	user.firstName = req.body.firstName;
+	user.lastName  = req.body.lastName;
+	user.setMail    (req.body.mail);
+	user.setTel     (req.body.tel);
+	
+	user.check(null, function(err, b) {
+		if (err) return next(err);
+		
+		if (b) {
+			user.save(function(err) {
+				if (err) return next(err);
+				
+				user.check(null, function(err, b) {
+					if (err) return next(err);
+					if (b) return next(new Err(400, 1303, "Erreur lors de la création du compte"));
+					res.json(200, _.omit(user.obj(true), 'pass', 'isAdmin', 'created', 'updated'));
+				});
+			});
+		}
+		else {
+			return next(new Err(400, 1302, "Le login choisi est déjà utilisé"));
+		}
+	});
+}
+
+//DELETE USER FUNCTIONS 
 function deleteUser(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Function deleteUser');
+	new User(req.params.id, function(err, target) {
+		if (err) return next(err);
+		if (req.params.id == null || target.firstName == null) {
+			return next(new Err(404, 1305, "L'utilisateur n'existe pas"));
+		}
+		target.remove();
+		res.send(200);
+	});
 }
-
 
 module.exports.user = {
+	getOne		: 	getUserInfos,
+	getAll		: 	getAll,
         signup          :       signup,
-	get		: 	getUserInfos,
         destroy 	: 	deleteUser
 };
