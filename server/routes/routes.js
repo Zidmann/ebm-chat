fa70b7d7defb356af9e389a63641eca8c4446e54
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2014-03-18 11:11:31 +0100

    Set better routes for REST ABM Chat.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,9 @@
 var register=function (app, ctrl, cas)
 {
-
 // START API DEFINITION
-
     //User APIs
-    app.get   ("/api/users",       ctrl.user.get);   	                //Get user list
-    app.get   ("/api/users/:id",   ctrl.user.get);		        //Get information on a specific user
+    app.get   ("/api/users",       ctrl.user.getAll);  	                //Get user list
+    app.get   ("/api/users/:id",   ctrl.user.getOne);		        //Get information on a specific user
     app.post  ("/api/users",       ctrl.user.signup);   	        //Create a new user on database
     app.delete("/api/users/:id",   ctrl.user.destroy);		        //Get information on a specific user
 
@@ -13,19 +11,22 @@ var register=function (app, ctrl, cas)
     app.post  ("/api/login/:door", ctrl.login.signin);		        //Connect to chat server website through a door (Classical, Gmail, Facebook, CAS, ...)
     app.delete("/api/login",       ctrl.login.signout);                 //Disconnect from chat server website
 
-    //Room and Messages APIs
-    app.get   ("/api/rooms",     ctrl.room.get);		        //Get room list on the chat
-    app.post  ("/api/rooms",     ctrl.room.create);		        //Create a new room on the chat
-    app.get   ("/api/rooms/:id", ctrl.room.getMsg);		        //Get information and messages on a specific room
-    app.post  ("/api/rooms/:id", ctrl.room.postMsg);		        //Write a message in a specifi room
-    app.delete("/api/rooms/:id", ctrl.room.destroy);		        //Destroy a specific room
+    //Room APIs
+    app.get   ("/api/rooms",       ctrl.room.getAll);		        //Get room list on the chat
+    app.get   ("/api/rooms/:id",   ctrl.room.getOne);		        //Get information on a specific room
+    app.post  ("/api/rooms",       ctrl.room.create);		        //Create a new room on the chat
+    app.delete("/api/rooms/:id",   ctrl.room.destroy);		        //Destroy a specific room
+
+    //Messages APIs
+    app.get   ("/api/rooms/:room/msg",     ctrl.msg.getAll);		//Get information on a specific room
+    app.get   ("/api/rooms/:room/msg/:id", ctrl.msg.getOne);		//Get information on a specific room
+    app.post  ("/api/rooms/:room/msg/",    ctrl.msg.post);		//Create a new room on the chat
+    app.delete("/api/rooms/:room/msg/:id", ctrl.msg.destroy);		//Destroy a specific room
 
     //File APIs
     //Note : To get a file just go to /files/<filename>
     app.post  ("/api/files",     ctrl.file.upload);		        //Upload from user to server a file
     app.delete("/api/files/:id", ctrl.file.destroy);		        //Delete a specific file
-
-
 }
 
 
