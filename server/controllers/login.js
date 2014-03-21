1b2450394f26da89e059d3a1fad6d5ea0569a3dc
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2014-03-21 21:25:18 +0100

    Set User Identification

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,22 +1,118 @@
-function cas(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Function UserInfos');
+var 	User  = require('../models/user.js'),
+	Token = require('../models/token.js'),
+	Err   = require('../utils/err.js'),
+	_     = require('underscore'),
+	u     = require('../utils/utils.js'),
+	async = require('async'),
+	jwt   = require('jwt-simple'),
+	conf  = require('../conf.js');
+
+//Generator of token
+function generateToken(userId, res, next) {
+    var token   = new Token();
+
+    if (userId == null)
+    {
+	return next(new Err(400, 1301, "Les paramètres envoyés pour générer le token ne sont pas complets"));
+    }	
+
+    token.check(null, function(err, b) {
+        if (err) return next(err);
+        if (b) {
+    	    token.save(function(err) {
+		    if (err) return next(err);
+    		    token.check(null, function(err, b) {
+			if (err) return next(err);
+				if (b) return next(new Err(400, 1303, "Erreur lors de la création du token"));
+			});
+		    });
+	    token.updateToken(jwt.encode({id : token.id, userId: userId, random : this.token}, conf.secretToken));
+	    res.json(200, _.omit(token.obj(true), 'lastActivity', 'loaded', 'id'));
+        }
+        else {
+	    return next(new Err(400, 1302, "Le token généré est déjà utilisé"));
+	}
+    });
 }
 
+//Sign up with classic method : user, password
+function signinClassic(req, res, next) {
+	if (req.body.login == null || req.body.pass == null) {
+		return next(new Err(400, 1002, "Les identifiants ne sont pas tous communiqués"));
+	}
+	
+	var r = db.collection(conf.database.userCollection).find({login: req.body.login, pass: u.sha1(req.body.pass)}, {_id : 1});
+	r.count(function(err, count) {
+		if(err) return next(err);
+		
+		if(count != 1) return next(new Err(400, 1101, "Les identifiants sont incorrects"));
+		
+		r.nextObject(function(err, doc) {
+			if (err) return next(err);
+			generateToken(doc._id, res, next);
+		});
+	});
+}
+
+//Function to decrypt a token
+function readToken(req, res, next) {
+    var decoded = jwt.decode(req.params.token, conf.secretToken);
+    res.json(200, decoded);
+}
+
+//Function to choose what method must be userd
 function signin(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Function UserInfos');
+	switch (req.params.door) {
+		//Use Classical connection with login and password
+		case "classic":
+			signinClassic(req, res, next);
+		break;
+
+		//Use connection with RFID card
+		case "rfid":
+			return next(new Err(400, 1301, "La méthode d'accès n'est pas encore implémentée."));						
+		break;
+
+		//Use connection with Google API
+		case "google":
+			return next(new Err(400, 1301, "La méthode d'accès n'est pas encore implémentée."));						
+		break;
+
+		//Use connection with Twitter API
+		case "facebook":
+			return next(new Err(400, 1301, "La méthode d'accès n'est pas encore implémentée."));			
+		break;
+
+		//Case where connection door is unknown
+		default: 
+			return next(new Err(400, 1301, "La méthode d'accès est inconnue"));
+		break;
+	}
 }
 
+//Function to remove token
 function signout(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Function UserInfos');
+    var decoded = jwt.decode(req.params.token, conf.secretToken);
+    if(decoded==null)
+    {
+	return next(new Err(404, 1305, "Le token est mal défini"));
+    }
+    else
+    {
+        new Token(decoded.id, function(err, target) {
+    	    if (err) return next(err);
+	    if (target.token==null) {
+		return next(new Err(404, 1305, "Le token n'existe pas"));
+	    }
+	    target.remove();
+	    res.send(200);
+        });
+    }
 }
-
 
 module.exports.login = {
-	cas		:	cas,
         signin          :       signin,
-        signout         :       signout
+        signout         :       signout,
+	readToken	:	readToken
 };
 
--- server/models/user.js
+++ server/models/user.js
@@ -7,28 +7,25 @@ var u        = require('../utils/utils.js'),
 
 function User(id, callback, obj) {	
 	id = (typeof id === 'string' && id.length == 24) ? u.ObjectID(id) : id;
-	
-	this.id        = id;
-	this.login     = null;
-	this.pass      = null;
-	this.avatar    = null;
-	this.civilite  = null;
-	this.firstName = null;
-	this.lastName  = null;
-	this.mail      = null;
-	this.tel       = null;
-	this.isAdmin   = false;
-	this.created   = new Date();
-	this.updated   = null;
+	this.id           = id;
+	this.login        = null;
+	this.pass         = null;
+	this.avatar       = null;
+	this.civilite     = null;
+	this.firstName    = null;
+	this.lastName     = null;
+	this.mail         = null;
+	this.tel          = null;
+	this.isAdmin      = false;
+	this.created      = new Date();
+	this.updated      = null;
 	
 	// Object properties
 	this.loaded = false;
-	
 	if (obj) {
 		this.fromObj(obj);
 		this.loaded = true;
 	}
-	
 	if (id != null && !this.loaded) this.fetch(callback);
 	else if (callback) callback(null, this);
 };
@@ -49,32 +46,36 @@ User.prototype.toString = function() {
 };
 
 User.prototype.fromObj = function(obj) {
-	this.login     = obj.login;
-	this.pass      = obj.pass;
-	this.avatar    = obj.avatar;
-	this.civilite  = obj.civilite;
-	this.firstName = obj.firstName;
-	this.lastName  = obj.lastName;
-	this.mail      = obj.mail;
-	this.tel       = obj.tel;
-	this.isAdmin   = obj.isAdmin;
-	this.created   = obj.created;
-	this.updated   = obj.updated;
+	this.login        = obj.login;
+	this.pass         = obj.pass;
+	this.avatar       = obj.avatar;
+	this.civilite     = obj.civilite;
+	this.firstName    = obj.firstName;
+	this.lastName     = obj.lastName;
+	this.mail         = obj.mail;
+	this.tel          = obj.tel;
+	this.isAdmin      = obj.isAdmin;
+	this.token        = obj.token;
+	this.lastActivity = obj.lastActivity;
+	this.created      = obj.created;
+	this.updated      = obj.updated;
 };
 
 User.prototype.obj = function(id) {
 	var o = {
-		login      : this.login,
-		pass       : this.pass,
-                avatar     : this.avatar,
-                civilite   : this.civilite,
-		firstName  : this.firstName,
-		lastName   : this.lastName,
-		mail       : this.mail,
-		tel        : this.tel,
-		isAdmin    : this.isAdmin,
-		created    : this.created,
-		updated    : this.updated
+		login        : this.login,
+		pass         : this.pass,
+                avatar       : this.avatar,
+                civilite     : this.civilite,
+		firstName    : this.firstName,
+		lastName     : this.lastName,
+		mail         : this.mail,
+		tel          : this.tel,
+		isAdmin      : this.isAdmin,
+		token        : this.token,
+		lastActivity : this.lastActivity,
+		created      : this.created,
+		updated      : this.updated
 	};
 	if (id) o.id = this.id;
 	return o;
@@ -86,7 +87,7 @@ User.prototype.remove = function() {
 }
 
 User.prototype.setPassword = function(pass) {
-	if (pass) this.pass = u.sha(pass);
+	if (pass) this.pass = u.sha1(pass);
 };
 
 User.prototype.setMail = function(mail) {
@@ -106,6 +107,14 @@ User.prototype.setTel = function(tel) {
 	}
 	return false;
 };
+
+User.prototype.isAuthorized = function(login, password, callback) {
+	if (login === null || password === null) return callback(null, false);
+	db.collection(conf.database.userCollection).find({login : login, pass : u.sha1(password)}).count(function(err, count) {
+		callback(err, count==1);
+	});
+};
+
 
 /* check : checks the availability of the parameters in the db
  * @param champ : name of the field to check
