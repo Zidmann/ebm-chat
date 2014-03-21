4f34e072b6b24f2bf9aa43ec81cc3f3e1f097392
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2014-03-21 21:26:03 +0100

    Update route file.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -12,31 +12,32 @@ var register=function (app, cas)
 {
 // START API DEFINITION
     //User APIs
-    app.get   ("/api/users",       ctrl.user.getAll);  	                //Get user list
-    app.get   ("/api/users/:id",   ctrl.user.getOne);		        //Get information on a specific user
-    app.post  ("/api/users",       ctrl.user.signup);   	        //Create a new user on database
-    app.delete("/api/users/:id",   ctrl.user.destroy);		        //Get information on a specific user
+    app.get   ("/api/users",       ctrl.user.getAll);  	      //PRIVATE          //Get user list
+    app.get   ("/api/users/:id",   ctrl.user.getOne);	      //PRIVATE          //Get information on a specific user
+    app.post  ("/api/users",       ctrl.user.signup);         //PUBLIC           //Create a new user on database
+    app.delete("/api/users/:id",   ctrl.user.destroy);	      //PRIVATE          //Get information on a specific user
 
     //Login APIs
-    app.get  ("/api/login/:door", ctrl.login.signin);		        //Connect to chat server website through a door (Classical, Gmail, Facebook, CAS, ...)
-    app.delete("/api/login",       ctrl.login.signout);                 //Disconnect from chat server website
-
+    app.get   ("/api/login/:token", ctrl.login.readToken);    //PUBLIC           //Get UserId with token
+    app.post  ("/api/login/:door",  ctrl.login.signin);	      //PUBLIC           //Connect to chat server website through a door (Classical, Gmail, Facebook, CAS, ...) and return a token
+    app.delete("/api/login/:token", ctrl.login.signout);      //PRIVATE          //Disconnect from chat server website by removing the token
+ 
     //Room APIs
-    app.get   ("/api/rooms",       ctrl.room.getAll);		        //Get room list on the chat
-    app.get   ("/api/rooms/:id",   ctrl.room.getOne);		        //Get information on a specific room
-    app.post  ("/api/rooms",       ctrl.room.create);		        //Create a new room on the chat
-    app.delete("/api/rooms/:id",   ctrl.room.destroy);		        //Destroy a specific room
+    app.get   ("/api/rooms",       ctrl.room.getAll);	      //PRIVATE          //Get room list on the chat
+    app.get   ("/api/rooms/:id",   ctrl.room.getOne);	      //PRIVATE          //Get information on a specific room
+    app.post  ("/api/rooms",       ctrl.room.create);	      //PRIVATE          //Create a new room on the chat
+    app.delete("/api/rooms/:id",   ctrl.room.destroy);	      //PRIVATE	         //Destroy a specific room
 
     //Messages APIs
-    app.get   ("/api/rooms/:room/msg",     ctrl.msg.getAll);		//Get all messages in a room
-    app.get   ("/api/rooms/:room/msg/:id", ctrl.msg.getOne);		//Get one specific message in a room
-    app.post  ("/api/rooms/:room/msg",     ctrl.msg.post);		//Publish a message on a room
-    app.delete("/api/rooms/:room/msg/:id", ctrl.msg.destroy);		//Destroy a specific message in a room
+    app.get   ("/api/rooms/:room/msg",     ctrl.msg.getAll);  //PRIVATE          //Get all messages in a room
+    app.get   ("/api/rooms/:room/msg/:id", ctrl.msg.getOne);  //PRIVATE          //Get one specific message in a room
+    app.post  ("/api/rooms/:room/msg",     ctrl.msg.post);    //PRIVATE          //Publish a message on a room
+    app.delete("/api/rooms/:room/msg/:id", ctrl.msg.destroy); //PRIVATE          //Destroy a specific message in a room
 
     //File APIs
     //Note : To get a file just go to /files/<filename>
-    app.post  ("/api/files",     ctrl.file.upload);		        //Upload from user to server a file
-    app.delete("/api/files/:id", ctrl.file.destroy);		        //Delete a specific file
+    app.post  ("/api/files",     ctrl.file.upload);	      //PRIVATE          //Upload from user to server a file
+    app.delete("/api/files/:id", ctrl.file.destroy);	      //PRIVATE          //Delete a specific file
 }
 
 
